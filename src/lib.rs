9a1a693dc579cca401ee36a3518f0cf5d859093f
jedireza <reza@akhavan.me>
2020-04-04 22:08:59 -0700

    starting over

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,1 @@
 //! A WARC (Web ARChive) library
-
-extern crate chrono;
-#[macro_use] extern crate hyper;
-extern crate uuid;
-
-pub mod header;
-mod warc_record;
-mod warc_version;
-
-pub use warc_record::WarcRecord;
-pub use warc_version::WarcVersion;
