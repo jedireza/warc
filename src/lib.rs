8bfc7d52b2a592468b2ebb0a5eb9d5d5fa5ccd90
jedireza <reza@akhavan.me>
2020-04-09 23:26:12 -0700

    lib api syntax

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
 //! A WARC (Web ARChive) library
 
 pub mod header;
-pub mod parser;
-mod record;
-mod record_type;
-mod truncated_type;
-
 pub use header::{WarcHeader, WarcHeaders};
+
+pub mod parser;
+
+mod record;
 pub use record::WarcRecord;
+
+mod record_type;
 pub use record_type::WarcRecordType;
+
+mod truncated_type;
 pub use truncated_type::WarcTruncatedType;
