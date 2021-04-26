8f61f00d46cf637c397388d527ecb57e760e045d
jhwgh1968 <jhwgh1968@protonmail.com>
2021-04-26 15:13:25 -0500

    Fix write_file example

--- examples/write_file.rs
+++ examples/write_file.rs
@@ -1,6 +1,6 @@
 use chrono::prelude::*;
 
-use warc::{BufferedBody, Record, RecordType, WarcWriter};
+use warc::{header::WarcHeader, BufferedBody, Record, RecordType, WarcWriter};
 
 fn main() -> Result<(), std::io::Error> {
     let date = Utc::now();
@@ -10,7 +10,9 @@ fn main() -> Result<(), std::io::Error> {
     let mut headers = Record::<BufferedBody>::new();
     headers.set_warc_type(RecordType::WarcInfo);
     headers.set_date(date);
-    headers.set_ip_address("127.0.0.1");
+    headers
+        .set_header(WarcHeader::IPAddress, "127.0.0.1")
+        .expect("BUG: should be a valid IP address");
     let record = headers.add_body(body);
 
     let mut file = WarcWriter::from_path("warc_example.warc")?;
